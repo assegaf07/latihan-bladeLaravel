e717ce4f00d8adfc5a60f8c4f332a9e05e2e8e3b
assegaf07 <59826182+assegaf07@users.noreply.github.com>
2020-07-01 18:29:02 +0700

    tugas blade

---
+++ resources/views/items/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.master')
+
+@section('content')
+    <h2>INI HALAMAN CREATE</h2>
+@endsection
+
+@push('script')
+<script>
+  var a = ["nama", "saya"];
+  alert(a);
+</script>
+@endpush
